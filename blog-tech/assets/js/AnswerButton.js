918a850e2ef2a72b9fd36435a58ed7a41a573877
Nathan Swindall <Nathan.Swindall20103@gmail.com>
2021-02-24 08:46:07 -0600

    worked on the Elm basics part

--- blog-tech/assets/js/AnswerButton.js
+++ blog-tech/assets/js/AnswerButton.js
@@ -2,15 +2,32 @@ class AnswerButton extends HTMLElement {
     connectedCallback() {
         var id = this.getAttribute('answer_target')
         var button = document.createElement('button')
+
+        // Styles
+        button.style.backgroundColor = '#a5e07b'
+        button.style.color = "#6b1e04"
+        button.style.borderRadius = "8px"
+        button.style.padding = "10px 12px"
+        button.style.transitionDuration= "0.8s"
+        button.style.fontWeight = "bold"
+        button.style.margin = "10px 0"
+        button.onmouseover = () => { button.style.backgroundColor = "white"}
+        button.onmouseout = () => { button.style.backgroundColor = "#a5e07b"}
+        button.style.cursor = "pointer"
+        button.style.outline = "none"
+        button.style.textDecoration = "none"
         button.innerText = `Answer`
+
+        // Set the event
         button.addEventListener('click', function () {
-            var x = document.getElementById(id)
-            if (x.style.display === "none") {
-                x.style.display = "block";
+            let answerDiv = document.getElementById(id)
+            if (answerDiv.style.display === 'none' || answerDiv.style.display === "") {
+                answerDiv.style.display = "block";
             } else {
-                x.style.display = "none";
+                answerDiv.style.display = 'none';
             }});
         this.appendChild(button)
+
         
   }
 }
